f9487f90125eaee2c55dab58f1f3705a4c921ae0
4lbatr0s <serhatoner@protonmail.com>
2023-05-29 02:10:12 +0300

    UPDATE Button style

--- frontend/src/examples/Tables/DataTable/DataTableBodyCell.js
+++ frontend/src/examples/Tables/DataTable/DataTableBodyCell.js
@@ -24,8 +24,8 @@ function DataTableBodyCell({ noBorder, align, children }) {
     <MDBox
       component="td"
       textAlign={align}
-      py={1.5}
-      px={1}
+      py={1}
+      px={2}
       sx={({ palette: { light }, typography: { size }, borders: { borderWidth } }) => ({
         fontSize: size.sm,
         borderBottom: noBorder ? "none" : `${borderWidth[1]} solid ${light.main}`,
